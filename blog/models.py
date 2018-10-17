9cb600cbe38ae5053a073e95512dfe1f51f14ef7
amitsharma-007 <34056932+amitsharma-007@users.noreply.github.com>
2018-10-17 13:27:03 +0530

    Blog final-1

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,14 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to = 'images/')
+    
+
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:200]
+    
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
@@ -8,5 +8,6 @@ import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name = 'home'),
+    path('blog/', include('blog.urls')),
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
